7ff4bdc71438cb002eab9238e6f1c658d81e5cac
Ytucayasi <saul.ytucayasi@gmail.com>
2023-08-06 18:07:48 -0500

    Pago a empleados

---
+++ resources/views/livewire/employee-list.blade.php
@@ -0,0 +1,131 @@
+<div class="flex flex-col gap-10">
+  <div class="flex justify-between">
+    <div class="flex flex-row gap-2">
+      <input placeholder="Ingrese el empleado" class="border-0 focus:border-white rounded-md shadow-md" type="text">
+      <button class="bg-[#314F9A] p-2 rounded-md text-white flex gap-2 items-center shadow-md">
+        <i class="fa fa-search"></i>
+        <span>Buscar</span>
+      </button>
+    </div>
+    <div class="flex flex-row gap-2">
+      <button class="bg-[#319848] p-2 rounded-md text-white flex gap-2 items-center shadow-md">
+        <i class="fa fa-search"></i>
+        <span>Importar Excel</span>
+      </button>
+      <button class="bg-[#9B34BF] p-2 rounded-md text-white flex gap-2 items-center shadow-md">
+        <i class="fa fa-search"></i>
+        <span>Exportar Excel</span>
+      </button>
+    </div>
+  </div>
+  <div class="flex justify-center items-center gap-4">
+    <button class="hover:bg-[#314F9A] p-2 rounded-md text-white flex gap-2 items-center">
+      <span>Todos</span>
+    </button>
+    <button class="hover:bg-[#314F9A] p-2 rounded-md text-white flex gap-2 items-center">
+      <span>Agregados</span>
+    </button>
+  </div>
+  <table class="bg-[#314F9A] rounded-md text-white overflow-hidden">
+    <thead class="bg-white text-black">
+      <tr>
+        <th class="p-5">DNI</th>
+        <th>N° de cuenta</th>
+        <th>Apellido paterno</th>
+        <th>Apellido materno</th>
+        <th>Nombres</th>
+        <th>Mod. de Contra.</th>
+      </tr>
+    </thead>
+    <tbody class="">
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+      <tr class="text-center cursor-pointer hover:bg-blue-500">
+        <td class="p-3">74701343</td>
+        <td>1119239283281</td>
+        <td>Ytucayasi Savina</td>
+        <td>Yupanqui Mendoza</td>
+        <td>Saul David Daniel Alonso</td>
+        <td>Presencial</td>
+      </tr>
+    </tbody>
+  </table>
+  <div class="flex justify-end gap-2">
+    <button class="bg-white text-black p-3 rounded-md flex gap-2 items-center shadow-md hover:bg-[#314F9A] hover:text-white">
+      <i class="fa fa-chevron-left"></i>
+    </button>
+    <button class="bg-white text-black p-3 rounded-md flex gap-2 items-center shadow-md hover:bg-[#314F9A] hover:text-white">
+      <i class="fa fa-chevron-right"></i>
+    </button>
+  </div>
+</div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,19 +1,12 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
-                    </a>
-                </div>
-
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-8 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Empleados') }}
                     </x-nav-link>
                 </div>
             </div>
